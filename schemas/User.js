b018a718951649f65a53d1308ccc6e1a30d99b36
hinanaz87 <119865555+hinanaz87@users.noreply.github.com>
2023-03-15 14:56:47 +0500

    version2

--- schemas/User.js
+++ schemas/User.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema({
     username:{type:String, required: true },
     email:{type:String, required: true },
     password:{type:String, required: true },
-    // confirmpassword:{type:String, required: true },
     phonenumber:{type:Number, required: true },
     role:{type:String, required: true },
     isBlocked:{type:Boolean, default: false },
